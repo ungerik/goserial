a4ac598203be56b802853b341f5bbd31120230bf
Erik Unger <erik@erikunger.com>
2014-11-28 15:47:40 +0100

    added Connection struct

--- serial.go
+++ serial.go
@@ -66,92 +66,27 @@ var (
 	ErrConfigParity   = errors.New("goserial config: bad parity")
 )
 
-type ParityMode byte
-
-const (
-	ParityNone = ParityMode(iota)
-	ParityEven
-	ParityOdd
-)
-
-type ByteSize byte
-
-const (
-	Byte8 = ByteSize(iota)
-	Byte5
-	Byte6
-	Byte7
-)
-
-type StopBits byte
-
-const (
-	StopBits1 = StopBits(iota)
-	StopBits2
-)
-
-// Config contains the information needed to open a serial port.
-//
-// Currently few options are implemented, but more may be added in the
-// future (patches welcome), so it is recommended that you create a
-// new config addressing the fields by name rather than by order.
-//
-// For example:
-//
-//    c0 := &serial.Config{Name: "COM45", Baud: 115200}
-// or
-//    c1 := new(serial.Config)
-//    c1.Name = "/dev/tty.usbserial"
-//    c1.Baud = 115200
-//
-type Config struct {
-	Name string
-	Baud int
-
-	Size     ByteSize
-	Parity   ParityMode
-	StopBits StopBits
-
-	// RTSFlowControl bool
-	// DTRFlowControl bool
-	// XONFlowControl bool
-
-	CRLFTranslate bool // Ignored on Windows.
-	// TimeoutStuff int
-}
-
-func (c *Config) check() error {
-	switch c.Size {
-	case Byte5, Byte6, Byte7, Byte8:
-	default:
-		return ErrConfigByteSize
-	}
-
-	switch c.StopBits {
-	case StopBits1, StopBits2:
-	default:
-		return ErrConfigStopBits
-	}
-
-	switch c.Parity {
-	case ParityNone, ParityEven, ParityOdd:
-	default:
-		return ErrConfigParity
-	}
-
-	return nil
+type Connection struct {
+	io.ReadWriteCloser
 }
 
 // OpenPort opens a serial port with the specified configuration
-func OpenPort(c *Config) (io.ReadWriteCloser, error) {
+func OpenPort(c *Config) (conn *Connection, err error) {
 	if err := c.check(); err != nil {
 		return nil, err
 	}
 
-	return openPort(c.Name, c)
+	var connection Connection
+	connection.ReadWriteCloser, err = openPort(c.Name, c)
+	if err != nil {
+		return nil, err
+	}
+
+	return &connection, nil
 }
 
 // func Flush()
+//  tcsetattr(fileDescriptor, TCSAFLUSH, &myNewTTYoptions);
 
 // func SendBreak()
 
